6c47f09038ada3760ccd27f30ec6b2e4cf95e78b
YaseenKhanPathan <yaseenpathan8088@gmail.com>
2024-06-20 21:37:25 +0530

    git command commit

--- hello.c
+++ hello.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 void main(){
   printf("Hello World\n");
-  printf("This message is from sub branch");
+  printf("This message is from sub branch\n");
+  printf("Git command usage");
 }
